92dc7dfc9bfe5844267d6df69ee633de0dbcd710
Bibhav Kumar Dubey <134066370+Bibhavxx@users.noreply.github.com>
2025-07-07 13:06:37 +0530

    Add files via upload

---
+++ Models/CoilData.cs
@@ -0,0 +1,16 @@
+﻿namespace CoilTrackingSystem.Models
+{
+    public class CoilData
+    {
+        public string CoilNo { get; set; }
+        public double Thickness { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public double Weight { get; set; }
+        public string Grade { get; set; }
+        public string TDC { get; set; }
+        public string PreviousStage { get; set; }
+        public string NextProcess { get; set; }
+        public string Process { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoilTrackingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
